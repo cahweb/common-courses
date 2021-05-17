9f7cddf3bee928eda19e754b4ec7a91ae0462ab3
rtucf <82979281+rtucf@users.noreply.github.com>
2021-05-17 19:44:47 -0400

    Added conditional check for duplicate functions.

--- includes/db.php
+++ includes/db.php
@@ -1,36 +1,42 @@
 <?
 
-function db_connect() {
-    $db_config = include "db_config.php";
+if (!function_exists('db_connect')) {
+    function db_connect() {
+        $db_config = include "db_config.php";
 
-    $db_server = $db_config["server"];
-    $db_user = $db_config["user"];
-    $db_pass = $db_config["pass"];
-    $db = $db_config["db"];
-    $db_charset = 'utf-8';
+        $db_server = $db_config["server"];
+        $db_user = $db_config["user"];
+        $db_pass = $db_config["pass"];
+        $db = $db_config["db"];
+        $db_charset = 'utf-8';
 
-    $db_connection = new mysqli($db_server, $db_user, $db_pass, $db);
+        $db_connection = new mysqli($db_server, $db_user, $db_pass, $db);
 
-    if ($db_connection -> connect_errno) {
-        echo "Failed to connect to " . $db_server . ": " . $mysqli -> connect_error;
-        exit();
+        if ($db_connection -> connect_errno) {
+            echo "Failed to connect to " . $db_server . ": " . $mysqli -> connect_error;
+            exit();
+        }
+
+        $db_connection->set_charset($db_charset);
+
+        return $db_connection;
     }
-
-    $db_connection->set_charset($db_charset);
-
-    return $db_connection;
 }
 
-function db_close($db_connection) {
-    $db_connection->close();
+if (!function_exists('db_close')) {
+    function db_close($db_connection) {
+        $db_connection->close();
+    }
 }
 
-function db_query($db_connection, $sql) {
-    if ($result = $db_connection->query($sql)) {
-        return $result->fetch_all(MYSQLI_ASSOC);
-    } else {
-        echo("Mysqli Error: " . $result -> error . "<br>");
-        return false;
+if (!function_exists('db_query')) {
+    function db_query($db_connection, $sql) {
+        if ($result = $db_connection->query($sql)) {
+            return $result->fetch_all(MYSQLI_ASSOC);
+        } else {
+            echo("Mysqli Error: " . $result -> error . "<br>");
+            return false;
+        }
     }
 }
 
